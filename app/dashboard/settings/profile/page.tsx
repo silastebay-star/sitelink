280283ae6b79b64cee380d0915dfc006662a7021
AquaFlowPro <info@aquaflowpro.co.uk>
2025-11-09 16:23:03 +0000

    Fix: Corrected relative import paths

--- app/dashboard/settings/profile/page.tsx
+++ app/dashboard/settings/profile/page.tsx
@@ -1,4 +1,4 @@
-import { Separator } from '../../../../components/ui/separator';
+import { Separator } from '../../../components/ui/separator';
 import ProfileForm from '@/components/profile-form'; // We will create this component next
 
 export default function ProfileSettingsPage() {
--- components/profile-form.tsx
+++ components/profile-form.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Button } from '../ui/button';
+import { Button } from './ui/button';
 import {
   Form,
   FormControl,
@@ -12,7 +12,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from '../ui/form';
+} from './ui/form';
 import { Input } from '@/components/ui/input';
 import { toast } from 'sonner';
 import { useEffect, useState } from 'react';

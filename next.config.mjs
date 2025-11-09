9d53525fd2d443e866c8064c775a0eea2358cc6e
AquaFlowPro <info@aquaflowpro.co.uk>
2025-11-09 16:39:34 +0000

    Feat: Integrate next-pwa for basic PWA functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,4 +17,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withPWA(nextConfig);
